ca98f946869e8515af879b49492c64ab53dcc1b1
Eris (borb) <remcosveen@gmail.com>
2025-08-08 22:51:26 +0200

    added pool checking thingamajig

--- main.lua
+++ main.lua
@@ -2,4 +2,17 @@
 to_big = to_big or function(x) return x end
 
 if not Dysnomia then Dysnomia = {} end
+
+function Card:is_category(pool)
+    if self.config.category and type(self.config.category) == "table" then
+        if self.config.category[pool] then
+            return true
+        end
+    elseif pool and type(pool) == "table" then
+        if pool[self.config.center.key] then
+            return true
+        end
+    end
+end
+
 assert(SMODS.load_file("lib/improvebans.lua"))()
